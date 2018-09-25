a0b2ea4611c11dfc4b24f5066ff127f075382134
lidong <lidong@zepplabs.com>
2018-09-25 18:20:56 +0800

    增加fifo

---
+++ src/typedef.h
@@ -0,0 +1,37 @@
+#ifndef TYPE_DEF_H
+#define TYPE_DEF_H
+
+
+
+typedef unsigned char   u8;
+typedef unsigned short   u16;
+typedef unsigned int   u32;
+typedef signed   char   int8;
+typedef unsigned char   uint8;
+typedef signed   short  int16;
+typedef unsigned short  uint16;
+//typedef signed   int    int32;
+//typedef unsigned int    uint32;
+//typedef signed   short  int16;
+//typedef unsigned short  uint16;
+//typedef signed   long   int32;
+//typedef unsigned long   uint32;
+typedef signed int int32;
+typedef unsigned int uint32;
+
+// Data
+typedef uint8       BYTE;
+
+// Unsigned numbers
+typedef uint8       UINT8;
+typedef uint16      UINT16;
+typedef uint32      UINT32;
+
+// Signed numbers
+typedef int8        INT8;
+typedef int16       INT16;
+typedef int32       INT32;
+
+
+
+#endif
